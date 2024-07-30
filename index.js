21b0cfb9270655e0842c3de55f2dbfb198300753
Emmanuel Obewu <114340234+Emmanuel-Obewu@users.noreply.github.com>
2024-07-30 11:32:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-function AddInputs()
-{
-    var total = 0;
-    var coll = document.getElementsByTagName("input")
-    for ( var i = 0; i<coll.length; ++i)
-    {
-        var ele = coll[i];
-        total += parseInt(ele.value);
+// function AddInputs()
+// {
+//     var total = 0;
+//     var collect = document.getElementsByTagName("input")
+//     for ( var i = 0; i<collect.length; ++i)
+//     {
+//         var ele = collect[i];
+//         total += parseInt(ele.value);
+//     }
+//     var Display = document.getElementById("Display");
+//     Display.innerHTML = "Your Output is " + total;
+// }
+
+
+
+document.getElementById('testCaseCount').addEventListener('change', function() {
+    const container = document.getElementById('testCasesContainer');
+    container.innerHTML = '';
+    const count = parseInt(this.value);
+    for (let i = 0; i < count; i++) {
+        container.innerHTML += `
+            <label for="testCase${i}">Test Case ${i + 1}:</label>
+            <input type="number" id="testCase${i}" min="1" max="1000000" required>
+            <br><br>
+        `;
     }
-    var Display = document.getElementById("Display");
-    if(Number){
-    Display.innerHTML = "Your Output is " + total;
+});
 
-} else{
-  console.log("Error")
-}
+
+function sumOfDigits(num) {
+    return num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
 }
 
+function calculateSum() {
+    const count = parseInt(document.getElementById('testCaseCount').value);
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '';
+    for (let i = 0; i < count; i++) {
+        const num = parseInt(document.getElementById(`testCase${i}`).value);
+        if (num >= 1 && num <= 1000000) {
+            const result = sumOfDigits(num);
+            resultsDiv.innerHTML += `<div class="result-item">Sum of digits for Test Case ${i + 1}: ${result}</div>`;
+        } else {
+            resultsDiv.innerHTML += `<div class="result-item">Test Case ${i + 1} is out of bounds.</div>`;
+        }
+    }
+}
 
 
-// Function to find Chef's lucky letter
 function findLuckyLetter() {
     const input = document.getElementById('inputString').value;
 
@@ -35,7 +63,7 @@ function findLuckyLetter() {
 
 
 
-   // Function for jewelry cost
+   
    function generateTestCaseInputs() {
     const numTests = document.getElementById('numTests').value;
     const container = document.getElementById('testCasesContainer');
@@ -75,3 +103,38 @@ function calculateCosts() {
 
 
 document.getElementById('numTests').addEventListener('change', generateTestCaseInputs);
+
+
+function validateCard() {
+    
+    var cardNumber = document.getElementById("cardNumber").value;
+
+    if (!/^\d{13,19}$/.test(cardNumber)) {
+        document.getElementById("result").innerText = "Invalid input! Please enter a number between 13 and 19 digits.";
+        return;
+    }
+
+    
+    var sum = 0;
+    var shouldDouble = false;
+
+    for (var i = cardNumber.length - 1; i >= 0; i--) {
+        var digit = parseInt(cardNumber.charAt(i), 10);
+
+        if (shouldDouble) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9; 
+            }
+        }
+
+        sum += digit;
+        shouldDouble = !shouldDouble; 
+    }
+
+    if (sum % 10 === 0) {
+        document.getElementById("result").innerText = "Valid";
+    } else {
+        document.getElementById("result").innerText = "Invalid";
+    }
+}
